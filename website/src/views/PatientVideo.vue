483cf28405b9946c7eed8acfb26ece0a556418a2
ianczm <ianczm@live.co.uk>
2023-10-08 22:55:13 +0800

    Format file

--- website/src/views/PatientVideo.vue
+++ website/src/views/PatientVideo.vue
@@ -1,26 +1,20 @@
 <template>
-    <div class="video-player">
-      <video ref="videoPlayer" controls>
-        <source :src="videoSrc" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        videoSrc: require('@/assets/therapy.mp4'), // Use a relative path to the video file
-      };
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .video-player {
-    width: 66%; /* 66% of the screen width */
-    float: left;
-    margin-top: 20px;
-  }
-  </style>
+  <div class="video-player">
+    <video ref="videoPlayer" controls>
+      <source :src="videoSrc" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      videoSrc: require('@/assets/therapy.mp4'), // Use a relative path to the video file
+    };
+  },
+};
+</script>
+
+<style scoped></style>
