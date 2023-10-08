dd32430dc6ec2242ead76aadb3c16296ad5a46ea
ianczm <ianczm@live.co.uk>
2023-10-08 22:39:30 +0800

    Format

--- website/src/views/VideoPlayer.vue
+++ website/src/views/VideoPlayer.vue
@@ -1,40 +1,39 @@
 <template>
-    <div>
-      <video-player :videoSrc="videoSrc" />
-      <patient-details
-        :patientName="patientName"
-        :patientAge="patientAge"
-        :appointmentDate="appointmentDate"
-        :doctorName="doctorName"
-      />
-      <patient-emotions :emotions="emotions" />
-    </div>
-  </template>
-  
-  <script>
-  import VideoPlayer from './PatientVideo.vue';
-  import PatientDetails from './PatientDetails.vue';
-  import PatientEmotions from './PatientEmotions.vue';
-  
-  export default {
-    components: {
-      VideoPlayer,
-      PatientDetails,
-      PatientEmotions,
-    },
-    data() {
-      return {
-        videoSrc: 'path/to/your/video.mp4',
-        patientName: 'John Doe',
-        patientAge: 30,
-        appointmentDate: '2023-10-02',
-        doctorName: 'Dr. Smith',
-        emotions: [
-          { emotion: 'Happy', start: '00:00', end: '00:50' },
-          // Add more emotion objects with timestamps as needed
-        ],
-      };
-    },
-  };
-  </script>
-  
+  <div>
+    <video-player :videoSrc="videoSrc" />
+    <patient-details
+      :patientName="patientName"
+      :patientAge="patientAge"
+      :appointmentDate="appointmentDate"
+      :doctorName="doctorName"
+    />
+    <patient-emotions :emotions="emotions" />
+  </div>
+</template>
+
+<script>
+import VideoPlayer from "./PatientVideo.vue";
+import PatientDetails from "./PatientDetails.vue";
+import PatientEmotions from "./PatientEmotions.vue";
+
+export default {
+  components: {
+    VideoPlayer,
+    PatientDetails,
+    PatientEmotions,
+  },
+  data() {
+    return {
+      videoSrc: "path/to/your/video.mp4",
+      patientName: "John Doe",
+      patientAge: 30,
+      appointmentDate: "2023-10-02",
+      doctorName: "Dr. Smith",
+      emotions: [
+        { emotion: "Happy", start: "00:00", end: "00:50" },
+        // Add more emotion objects with timestamps as needed
+      ],
+    };
+  },
+};
+</script>
