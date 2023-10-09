3ffdd642d53c732cbdb21f2972f15b52eb3f47fd
dchi0021 <dchi0021@student.monash.edu>
2023-10-10 06:59:20 +0800

    change video source to url fetched

--- website/src/views/PatientVideo.vue
+++ website/src/views/PatientVideo.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="video-player">
     <video ref="videoPlayer" controls autoplay>
-      <source
-        :src="getVideoUrl()"
-      />
+      <source :src="videoSrc" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   </div>
@@ -11,16 +9,21 @@
 
 <script>
 export default {
+  props: {
+    videoSrc: String,
+  },
   methods: {
     getVideoUrl() {
       // Construct the URL for the video based on the videoKey
-      const bucketUrl = 'https://mcs21fyp.s3.amazonaws.com/';
+      const bucketUrl = "https://mcs21fyp.s3.amazonaws.com/";
       return `${bucketUrl}${this.$route.params.videoKey}`;
     },
   },
   mounted() {
     // Expose the videoPlayer instance as a property
-    this.$emit('video-player', this.$refs.videoPlayer);
+    this.$emit("video-player", this.$refs.videoPlayer);
+    console.log("Video player mounted");
+    console.log(this.videoSrc);
   },
 };
 </script>
